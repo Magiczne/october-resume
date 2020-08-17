5c965c388fa9784844818fd19d4a06204c83a1ac
Michał Kleszczyński <michalkamilkleszczynski@gmail.com>
2020-08-17 18:08:42 +0200

    Added english translations

--- lang/en/lang.php
+++ lang/en/lang.php
@@ -1,11 +1,55 @@
 <?php
 
 return [
+    'components' => [
+        'resume' => [
+            'name' => 'Resume',
+            'description' => 'Display your resume'
+        ]
+    ],
+    'fields' => [
+        'content' => 'Content',
+        'description' => 'Description',
+        'icon' => 'Icon',
+        'language' => 'Language',
+        'mail' => 'Mail',
+        'social_name' => 'Social network',
+        'time_period' => 'Time period',
+        'title' => 'Title',
+        'url' => 'URL',
+        'value' => 'Value'
+    ],
+    'form' => [
+        'edit' => 'Edit',
+        'preview' => 'Preview'
+    ],
+    'menu' => [
+        'education' => 'Education',
+        'experience' => 'Experience',
+        'info' => 'Info',
+        'languages' => 'Languages',
+        'socials' => 'Socials'
+    ],
+    'messages' => [
+        'created' => 'New entry has been created',
+        'deleted' => 'Entry has been deleted',
+        'updated' => 'Entry has been updated'
+    ],
     'plugin' => [
         'name' => 'Resume',
-        'description' => 'Add resume to your webpage'
+        'description' => 'Add resume to your website'
     ],
     'permissions' => [
         'manage-resume' => 'Manage resume'
+    ],
+    'settings' => [
+        'fields' => [
+            'name' => 'Name',
+            'comment' => 'Type your name here. It will be displayed in the CV'
+        ],
+        'gdpr' => [
+            'label' => 'Consent to the processing of personal data',
+            'comments' => 'Enter the consent text. It will be displayed in the CV'
+        ]
     ]
 ];
